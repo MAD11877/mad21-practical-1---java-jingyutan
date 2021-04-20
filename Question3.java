afc9cbb7aee04783acaf1acd5cfa3924aae0fb01
jingyu <77566547+jingyutan@users.noreply.github.com>
2021-04-20 17:40:44 +0800

    Added answer

--- Question3.java
+++ Question3.java
@@ -10,7 +10,14 @@ public class Question3
      * Hint: in.nextInt() to read integer
      */
      
-    Scanner in = new Scanner(System.in);
+    Scanner in = new Scanner(System.in); // Creating scanner object "in"
+    
+    System.out.print("Enter number: ");
+    double num = in.nextInt(); // Asssigning enterred user value into "num"
+
+    num = num * num;
+
+    System.out.print("Result is " + num);
     
   }
 }
