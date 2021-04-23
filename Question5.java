a252b2adec0d559aab3fc2b236021d4e65636cfd
jingyu <>
2021-04-23 03:28:25 +0000

    Finish qn5

--- Question5.java
+++ Question5.java
@@ -1,4 +1,6 @@
+import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Question5
 {
@@ -25,10 +27,41 @@ public class Question5
      *     2
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
-     
-    Scanner in = new Scanner(System.in);
-    System.out.print("Enter number: ");
+
+   Scanner in = new Scanner(System.in);
+    
     int num = in.nextInt();
+    int mode = 0;
+    int count = 0;
+    int arr[] = new int[num];
+
+    
+    for (int i = 0; i < num; i++) {
+      int number = in.nextInt();
+      arr[i] = number;
+    }
+
+
+    for (int x = 0; x < arr.length; x++){
+      int tempcount = 0; // reset to 0 after each number
+
+      for (int j = 0; j < arr.length; j++) {
+          if (arr[x] == arr[j]){
+            tempcount += 1;
+          }
+      }
+
+      if (tempcount > count) {
+        count = tempcount;
+        mode = arr[x];
+      }
+
+    }
+
+    System.out.print(mode);
+   
+  
+    
 
     
     
